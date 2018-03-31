0e8a1f0db51ef6026f4703a2111460046b79a2eb
kino1134 <inoue1134@gmail.com>
2018-04-01 04:06:12 +0900

    メッセージAPIの要らない処理を削除

--- server/api/messages.js
+++ server/api/messages.js
@@ -3,86 +3,6 @@ import Message from '../models/Message.js'
 
 const router = Router()
 
-const messages = [
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       'あいうえお',
-    posted:     new Date(2017,0,15,22,30,21),
-  },
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       '* <b>あいうえお</b>\nかきくけこ',
-    posted:     new Date(),
-  },
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       'あいうえお',
-    posted:     new Date(2017,0,15,22,30,21),
-  },
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       'あいうえお',
-    posted:     new Date(2017,0,15,22,30,21),
-  },
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       'あいうえお',
-    posted:     new Date(2017,0,15,22,30,21),
-  },
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       'あいうえお',
-    posted:     new Date(2017,0,15,22,30,21),
-  },
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       'あいうえお',
-    posted:     new Date(2017,0,15,22,30,21),
-  },
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       'あいうえお',
-    posted:     new Date(2017,0,15,22,30,21),
-  },
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       'あいうえお',
-    posted:     new Date(2017,0,15,22,30,21),
-  },
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       'あいうえお',
-    posted:     new Date(2017,0,15,22,30,21),
-  },
-  {
-    user_name:  'John Smith',
-    user_id:    '@test_example',
-    room_id:    '1',
-    body:       'あいうえお',
-    posted:     new Date(2017,0,15,22,30,21),
-  },
-]
-
 /* GET users listing. */
 router.get('/messages', function (req, res, next) {
   Message.find({}, (err, messages) => {
@@ -92,7 +12,6 @@ router.get('/messages', function (req, res, next) {
       res.json(messages)
     }
   })
-  // res.json(messages)
 })
 
 /* GET users listing. */
@@ -104,24 +23,13 @@ router.get('/messages/:room_id', function (req, res, next) {
       res.json(messages)
     }
   })
-  // res.json(messages)
-})
-
-/* GET user by ID. */
-router.get('/messages/:id', function (req, res, next) {
-  const id = parseInt(req.params.id)
-  if (id >= 0 && id < users.length) {
-    res.json(messages[id])
-  } else {
-    res.sendStatus(404)
-  }
 })
 
 router.post('/messages', function(req, res, next) {
   let message = new Message(Object.assign({
     posted: new Date(),
-    user_id: 'no_name',
-    user_name: 'ななし'
+    user_id: req.user.user_id,
+    user_name: req.user.user_name
   }, req.body))
 
   message.save((err, obj) => {
