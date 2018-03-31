6709a7f9bb3943fbfd811326bc04021f648d0d25
kino1134 <inoue1134@gmail.com>
2018-04-01 03:43:19 +0900

    Userモデル追加

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+const schema = new mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+    unique: true,
+    match: /^[a-zA-Z0-9_]+$/
+  },
+  user_name: {
+    type: String
+  },
+  e_mail: String,
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+schema.statics.authenticate = function (user_id, password, done) {
+  model.findOne({ user_id: user_id }, function (err, user) {
+    if (err) { return done(err) }
+    if (!user) { return done(null, false) }
+    if (user.password !== password) { return done(null, false) } // パスワードを平文で入れている
+    return done(null, user)
+  })
+}
+schema.statics.serializeUser = function (user, done) {
+  done(null, user.id)
+}
+schema.statics.deserializeUser = function (id, done) {
+  model.findById(id, function (err, user) {
+    done(err, user)
+  })
+}
+
+const model = mongoose.model('User', schema)
+
+export default model
