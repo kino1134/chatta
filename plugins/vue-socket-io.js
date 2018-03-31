d86da2ad63c94484f315ba5c2f9af56ff904592e
kino1134 <inoue1134@gmail.com>
2018-04-01 03:46:06 +0900

    socket.io-clientをカスタマイズ出来るようにする

--- plugins/vue-socket-io.js
+++ plugins/vue-socket-io.js
@@ -1,4 +1,22 @@
 import Vue from 'vue'
+import client from 'socket.io-client'
 import VueSocketio from 'vue-socket.io'
+// import * as Cookies from 'tiny-cookie'
 
-Vue.use(VueSocketio, 'http://localhost:3000')
+const socket = client('http://localhost:3000', {
+  // query: {
+  //   'api.sid': Cookies.get('connect.sid')
+  // }
+})
+
+socket.on('connect', () => {
+  console.log(socket.id)
+})
+socket.on('ping', () => {
+  console.log('ping')
+})
+socket.on('pong', () => {
+  console.log('pong')
+})
+
+Vue.use(VueSocketio, socket)
