f17f992372456f88b8aee7231e27c57d4cdab458
kino1134 <inoue1134@gmail.com>
2018-04-08 05:27:52 +0900

    Roomスキーマを追加

---
+++ server/models/Room.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const schema = new mongoose.Schema({
+  room_id: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+    unique: true,
+    match: /^[a-zA-Z0-9_.-]+$/
+  },
+  room_name: String,
+  room_type: {
+    type: String,
+    required: true,
+    enum: ['public', 'private', 'direct']
+  },
+  description: String,
+})
+
+export default mongoose.model('Room', schema)
