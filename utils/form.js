1fc9746f98b0b1aad76b2c1251863e22fba65dd7
kino1134 <inoue1134@gmail.com>
2018-04-07 20:16:26 +0900

    2度押し防止用の共通処理を準備する。

---
+++ utils/form.js
@@ -0,0 +1,35 @@
+export function preventDoubleSubmission (fn) {
+  return function (event) {
+    const el = event.target
+    let ret
+
+    isLoading(el, true)
+    try {
+      ret = fn.call(this, event)
+      if (isPromise(ret)) {
+        ret
+          .then(() => { isLoading(el, false) })
+          .catch(() => { isLoading(el, false) })
+      }
+      return ret
+    } finally {
+      if (!isPromise(ret)) {
+        isLoading(el, false)
+      }
+    }
+  }
+}
+
+function isPromise (obj) {
+  return typeof obj.then === 'function' && typeof obj.catch === 'function'
+}
+
+function isLoading (el, arg) {
+  if (arg) {
+    el.classList.add('is-loading')
+    el.disabled = true
+  } else {
+    el.classList.remove('is-loading')
+    el.disabled = false
+  }
+}
