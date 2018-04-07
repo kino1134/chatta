50f3967fd652f512aa331bfed7bb526bf40c735e
kino1134 <inoue1134@gmail.com>
2018-04-08 05:42:51 +0900

    ルームの追加処理を実装

--- pages/rooms/index/create.vue
+++ pages/rooms/index/create.vue
@@ -5,15 +5,40 @@
         <h2 class="title">
           新しいルームを作成します
         </h2>
+        <TopErrorMessage :errors="errors"/>
         <div class="field">
           <div class="control">
             <label class="label">
-              ルームID
+              ルームID<span class="tag is-medium is-info required">必須</span>
             </label>
-            <input class="input" type="text">
+            <input v-model="room_id" class="input" :class="errors.class('room_id')" type="text">
+            <InputErrorMessage :msg="errors.msg('room_id')"/>
             <p class="help">
-              何かしらの制約を付ける予定です。
+              英数字、一部の記号(_.-)のみ使用できます。
             </p>
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <label class="label">
+             種類
+            </label>
+            <input v-model="room_type" id="type_public" value="public" class="is-checkradio" type="radio">
+            <label class="label" for="type_public">
+             公開
+            </label>
+            <input v-model="room_type" id="type_private" value="private" class="is-checkradio" type="radio">
+            <label class="label" for="type_private">
+             招待制
+            </label>
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <label class="label">
+             名前
+            </label>
+            <input v-model="room_name" class="input" type="text">
           </div>
         </div>
         <div class="field">
@@ -21,7 +46,7 @@
             <label class="label">
               説明
             </label>
-            <input class="input" type="text">
+            <input v-model="description" class="input" type="text">
           </div>
         </div>
         <div class="field">
@@ -37,11 +62,28 @@
 </template>
 
 <script>
+import axios from '~/plugins/axios'
 import { preventDoubleSubmission } from '~/utils/form'
+import ErrorJson from '~/utils/ErrorJson'
+import TopErrorMessage from '~/components/TopErrorMessage'
+import InputErrorMessage from '~/components/InputErrorMessage'
 
 export default {
+  components: {
+    TopErrorMessage,
+    InputErrorMessage
+  },
   transition: {
     name: 'fade'
+  },
+  data () {
+    return {
+      errors: new ErrorJson(),
+      room_id: '',
+      room_type: 'public',
+      room_name: '',
+      description: ''
+    }
   },
   head () {
     return {
@@ -50,9 +92,16 @@ export default {
   },
   methods: {
     create: preventDoubleSubmission(function (event) {
-      console.log('wrap!')
-      console.log(event)
-      console.log(this)
+      return axios.post('/api/rooms', {
+        room_id: this.room_id,
+        room_type: this.room_type,
+        room_name: this.room_name,
+        description: this.description
+      }).then((res) => {
+        this.$router.push('/rooms/' + res.data.room_id)
+      }).catch((err) => {
+        this.errors = new ErrorJson(err.response.data)
+      })
     })
   }
 }
--- server/api/rooms.js
+++ server/api/rooms.js
@@ -1,4 +1,7 @@
 import { Router } from 'express'
+import Room from '../models/Room'
+import User from '../models/User'
+import { validate, notBlank } from '../utils/validate'
 
 const router = Router()
 
@@ -43,5 +46,32 @@ router.get('/rooms/:id', function (req, res, next) {
     res.sendStatus(404)
   }
 })
+
+router.post('/rooms',
+  ...validate([
+    notBlank('room_id')
+    .matches(/^[a-z0-9_.-]+$/i).withMessage('使用できない文字が含まれています。')
+    .not().isIn(['create']).withMessage('予約されています。')
+    .custom((value) => {
+      return Room.findOne({ room_id: value }).then(room => {
+        if (room) { throw new Error('exists Room') }
+      })
+    }).withMessage('すでに使われているIDです。'),
+    notBlank('room_type').isIn(['public', 'private', 'direct'])
+  ]),
+  function(req, res, next) {
+    const room = new Room(Object.assign({}, req.body))
+    room.save((err, obj) => {
+      if (err) {
+        res.status(500).json(err)
+      } else {
+        req.user.room_list.push(obj.room_id)
+        req.user.save((err_u, obj_u) => {
+          res.json(obj)
+        })
+      }
+    })
+})
+
 
 export default router
