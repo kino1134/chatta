88f5f862ab08180c7d7b3a22cf991fce4397c55f
kino1134 <inoue1134@gmail.com>
2018-04-07 20:26:48 +0900

    認証処理にバリデーションを追加

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import passport from 'passport'
+import { check } from 'express-validator/check'
+import { validate, notBlank } from '../utils/validate'
 
 const router = Router()
 
 router.post('/auth/local',
+  ...validate([notBlank('user_id'), notBlank('password')]),
   passport.authenticate('local', {  }),
   function(req, res, next) {
     req.session.save((err) => {
