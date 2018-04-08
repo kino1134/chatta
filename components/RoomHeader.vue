4039396460780029c8493bfaedb708d1902c8124
kino1134 <inoue1134@gmail.com>
2018-04-08 17:43:13 +0900

    お気に入り設定を変更するインターフェースを実装

--- components/RoomHeader.vue
+++ components/RoomHeader.vue
@@ -3,17 +3,19 @@
     <a class="room-burger">
       <i class="fa fa-bars fa-lg"></i>
     </a>
-    <a class="toggle-star" :class="{ on: isFavorite }"><i class="fa-star fa-lg" :class="{ fa: isFavorite, far: !isFavorite }"></i></a>
+    <a @click="toggleFavorite" class="toggle-star tooltip is-tooltip-bottom" :class="{ on: isFavorite }" :data-tooltip="isFavorite ? 'お気に入りから外す' : 'お気に入りに追加'">
+      <i class="fa-star fa-lg" :class="{ fa: isFavorite, far: !isFavorite }"></i>
+    </a>
     <div class="room-info">
       {{ this.roomName }}
     </div>
     <div class="spacer"></div>
     <div class="room-action">
       <a class="action-link tooltip is-tooltip-bottom" data-tooltip="ルーム情報"><i class="fa fa-info-circle fa-lg"></i></a>
-      <a class="action-link"><i class="fa fa-search fa-lg"></i></a>
-      <a class="action-link"><i class="fa fa-users fa-lg"></i></a>
-      <a class="action-link"><i class="fa fa-user-plus fa-lg"></i></a>
-      <a class="action-link"><i class="fa fa-ellipsis-v fa-lg"></i></a>
+      <a class="action-link tooltip is-tooltip-bottom" data-tooltip="メッセージ検索"><i class="fa fa-search fa-lg"></i></a>
+      <a class="action-link tooltip is-tooltip-bottom" data-tooltip="所属ユーザ一覧"><i class="fa fa-users fa-lg"></i></a>
+      <a class="action-link tooltip is-tooltip-bottom" data-tooltip="ユーザ招待"><i class="fa fa-user-plus fa-lg"></i></a>
+      <a class="action-link tooltip is-tooltip-bottom" data-tooltip="その他"><i class="fa fa-ellipsis-v fa-lg"></i></a>
     </div>
   </header>
 </template>
@@ -27,8 +29,7 @@ export default {
   },
   data () {
     return {
-      roomName: 'ホーム',
-      isFavorite: false
+      roomName: 'ホーム'
     }
   },
   mounted () {
@@ -43,6 +44,20 @@ export default {
   head () {
     return {
       title: this.roomName
+    }
+  },
+  computed: {
+    isFavorite () {
+      const favoriteList = this.$store.getters['user/loginUser'].room_list.filter((x) => x.favorite)
+      return favoriteList.find((x) => x.room_id === this.roomId)
+    }
+  },
+  methods: {
+    toggleFavorite () {
+      this.$store.dispatch({
+        type: 'user/toggleFavorite',
+        roomId: this.roomId
+      })
     }
   }
 }
